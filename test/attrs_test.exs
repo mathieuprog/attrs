32419aa3e6a1ad7fef4accc2725b1b83f949fe82
mathieuprog <5883963+mathieuprog@users.noreply.github.com>
2023-06-27 05:22:40 +0200

    Test raising of ArgumentError

--- test/attrs_test.exs
+++ test/attrs_test.exs
@@ -7,18 +7,30 @@ defmodule AttrsTest do
     assert Attrs.get(%{foo: 1}, :foo, 0) == 1
     assert Attrs.get(%{"foo" => 1}, :foo, 0) == 1
     assert Attrs.get(%{"bar" => 1}, :foo, 0) == 0
+
+    assert_raise ArgumentError, ~r"atom", fn ->
+      Attrs.get(%{}, "foo")
+    end
   end
 
   test "has?" do
     assert Attrs.has?(%{foo: 1}, :foo)
     assert Attrs.has?(%{"foo" => 1}, :foo)
     refute Attrs.has?(%{bar: 1}, :foo)
+
+    assert_raise ArgumentError, ~r"atom", fn ->
+      Attrs.has?(%{}, "foo")
+    end
   end
 
   test "put" do
     assert Attrs.put(%{"foo" => 1}, :bar, 2) == %{"foo" => 1, "bar" => 2}
     assert Attrs.put(%{foo: 1}, :bar, 2) == %{foo: 1, bar: 2}
     assert Attrs.put(%{}, :foo, 1) == %{foo: 1}
+
+    assert_raise ArgumentError, ~r"atom", fn ->
+      Attrs.put(%{}, "foo", 1)
+    end
   end
 
   test "merge" do
